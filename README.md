90b4c7b71f2b7eb9292f6a0dfe4c20ffc914c55f
Rαy <ray@Rays-MBP.pfsense.int>
2020-02-09 04:36:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cars-Ships_Position
---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+$('.shape').on('mouseenter touchstart', function () {
+    $(this).removeClass('img-hidden');
+    $(this).addClass('img-visible');
+});
+
+$('#main').on('mouseenter touchstart', function () {
+    $('.shape').removeClass('img-visible');
+    $('.shape').addClass('img-hidden');
+});
+
+$('body').mousemove(function (event) {
+    // console.log(`left: ${event.pageX-4}px; top: ${event.pageY-4}px;`);
+});
+
+let lastMainW = 1237;
+const fixPosition = function () {
+    let mainW = $('#main').width();
+    // let winH = $(window).height();
+    // if(mainW < winH){
+    //    $('#main').width(winH);
+    // } else {
+    //     $('#main').width($(window).width());
+    // }
+    $('.shape').each(function() {
+        const thisTop = parseFloat($(this).css('top').split('px')[0]);
+        const fixedTop = thisTop * mainW / lastMainW;
+        const thisLeft = parseFloat($(this).css('left').split('px')[0]);
+        const fixedLeft = thisLeft * mainW / lastMainW;
+        $(this).css('top', `${fixedTop}px`);
+        $(this).css('left', `${fixedLeft}px`);
+    });
+    lastMainW = mainW;
+};
+fixPosition();
+
+$(window).resize(function () {
+    fixPosition();
+});
